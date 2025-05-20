8a005a177b5063c5bd724c3cd814fe6af961adb3
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-05-20 20:32:32 +0500

    Create roman.js

---
+++ roman.js
@@ -0,0 +1,44 @@
+class Roman {
+	constructor(number) {
+		this.number = Math.floor(number);
+		this.roman = "";
+		if (number < 4000) {
+			while (number >= 1000) {
+				this.roman += "M";
+				number -= 1000;
+			};
+			while (number >= 500) {
+				this.roman += "D";
+				number -= 500;
+			};
+			while (number >= 100) {
+				this.roman += "C";
+				number -= 100;
+			};
+			while (number >= 50) {
+				this.roman += "L";
+				number -= 50;
+			};
+			while (number >= 10) {
+				this.roman += "X";
+				number -= 10;
+			};
+			while (number >= 5) {
+				this.roman += "V";
+				number -= 5;
+			};
+			while (number >= 1) {
+				this.roman += "I";
+				number -= 1;
+			};
+			this.roman = this.roman.replace("DCCCC","CM")
+			this.roman = this.roman.replace("CCCC","CD")
+			this.roman = this.roman.replace("LXXXX","XC")
+			this.roman = this.roman.replace("XXXX","XL")
+			this.roman = this.roman.replace("VIIII","IX")
+			this.roman = this.roman.replace("IIII","IV")
+		} else {
+			this.roman = `${roman(this.number/1000)}→${roman(this.number%1000)}`
+		};
+	}
+};
